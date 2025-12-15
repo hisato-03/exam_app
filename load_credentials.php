1c766a4e84793b25e910b328e357407d05c2ad48
hisato-3 <your_email@example.com>
2025-12-16 08:35:11 +0900

    Add credential restoration and update render.yaml

--- load_credentials.php
+++
@@ -1,8 +0,0 @@
-<?php
-function restore_credentials($envKey) {
-    $base64 = getenv($envKey);
-    if ($base64) {
-        $json = base64_decode($base64);
-        file_put_contents(__DIR__ . '/credentials.json', $json);
-    }
-}
